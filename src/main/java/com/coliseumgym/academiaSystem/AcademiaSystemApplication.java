7585fb543fe01d880c2e6a40881eae29fd0746e8
Arthur-silvas <arthurpompollo@gmail.com>
2025-09-15 17:18:03 -0300

    first commit

---
+++ src/main/java/com/coliseumgym/academiaSystem/AcademiaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.coliseumgym.academiaSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coliseumgym/academiaSystem/AcademiaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coliseumgym.academiaSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
